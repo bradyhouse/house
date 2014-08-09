637929864402a018c878e677513088b73fe6c144
bradyhouse <bradyhouse@gmail.com>
2014-08-09 13:42:33 -0500

    201408091342

--- web/build/temp/development/House/sass/config.rb
+++
@@ -1,2 +0,0 @@
-require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
-require '../../../../../house/sass/config.rb'
--- web/build/temp/development/House/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/e13542/github/house/web/house/app.js
