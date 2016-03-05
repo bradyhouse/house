1a014ad88b4a43f37f1b1e69a4aa8403a90db531
bradyhouse <bradyhouse@gmail.com>
2016-03-05 12:09:15 -0600

    201603020420

--- fiddles/d3/fiddle-0008-ObjectBinding/README.markdown
+++ fiddles/d3/fiddle-0008-ObjectBinding/README.markdown
@@ -6,7 +6,7 @@ fiddle-0008-ObjectBinding
 
 ### Title
 
-fiddle-0008-ObjectBinding
+Object Binding
 
 
 ### Creation Date
@@ -21,7 +21,9 @@ Chicago, IL
 
 ### Description
 
-Another d3 fiddle.
+Fiddle based on the [object-as-data example](https://github.com/NickQiZhu/d3-cookbook/blob/master/src/chapter3/object-as-data.html)
+given in chapter 3 of Nick Qi Zhu's book **Data Visualization with D3.js Cookbook**.  It demonstrates how to
+bind an array of objects to a simple bar chart.
 
 
 ### Published Version Link
@@ -31,4 +33,4 @@ N/A
 
 ### Tags
 
-d3
+d3, domain, range, select, selectAll, data, enter, append, exit, remove
--- fiddles/d3/fiddle-0008-ObjectBinding/app.js
+++ fiddles/d3/fiddle-0008-ObjectBinding/app.js
@@ -1,17 +1,17 @@
 (function (app, $, undefined) {
     "use strict";
-    var data = [ // <- A
-        {width: 10, color: 23},{width: 15, color: 33},
-        {width: 30, color: 40},{width: 50, color: 60},
-        {width: 80, color: 22},{width: 65, color: 10},
-        {width: 55, color: 5},{width: 30, color: 30},
-        {width: 20, color: 60},{width: 10, color: 90},
+    var data = [
+        {width: 10, color: 23}, {width: 15, color: 33},
+        {width: 30, color: 40}, {width: 50, color: 60},
+        {width: 80, color: 22}, {width: 65, color: 10},
+        {width: 55, color: 5}, {width: 30, color: 30},
+        {width: 20, color: 60}, {width: 10, color: 90},
         {width: 8, color: 10}
     ];
 
     var colorScale = d3.scale.linear()
-        .domain([0, 100])
-        .range(["#add8e6", "blue"]); // <- B
+        .domain([0, 10])
+        .range(["#add8e6", "blue"]);
 
     function render(data) {
         d3.select("body").selectAll("div.h-bar")
@@ -27,15 +27,15 @@
         d3.select("body").selectAll("div.h-bar")
             .data(data)
             .attr("class", "h-bar")
-            .style("width", function (d) { // <- C
-                return (d.width * 5) + "px"; // <- D
+            .style("width", function (d) {
+                return (d.width * 5) + "px";
             })
-            .style("background-color", function(d){
-                return colorScale(d.color); // <- E
+            .style("background-color", function (d) {
+                return colorScale(d.color);
             })
             .select("span")
             .text(function (d) {
-                return d.width; // <- F
+                return d.width;
             });
     }
 
@@ -47,7 +47,7 @@
         data.shift();
         data.push({width: randomValue(), color: randomValue()});
         render(data);
-    }, 150);
+    }, 1000);
 
     render(data);
 })(window.app = window.app || {}, d3)
