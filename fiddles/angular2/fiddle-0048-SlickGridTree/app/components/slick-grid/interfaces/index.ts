0253bfdfcdc7db2c3a2a8816ca2636064edacba6
bradyhouse <bradyhouse@gmail.com>
2017-04-25 08:12:19 -0500

    closes #157, closes #56, closes #158, closes #160, closes #158

--- fiddles/angular2/fiddle-0048-SlickGridTree/app/components/slick-grid/interfaces/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './tree-grid-changes.interface';
-export * from './tree-grid-click-type.enum';
-export * from './tree-grid-column.interface';
-export * from './tree-grid-events.enum';
-export * from './tree-grid-options.interface';
