50b2d92c03627ab48da2d39e8a0753d25046af93
bradyhouse@gmail.com <bradyhouse@gmail.com>
2017-11-28 17:36:44 -0600

    20171128173626

--- fiddles/angular2-cli/fiddle-0014-FacebookAuthO/mean-rsvp/server/config.js
+++ fiddles/angular2-cli/fiddle-0014-FacebookAuthO/mean-rsvp/server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  AUTH0_DOMAIN: '[bradyhouse.auth0.com]', // e.g., kmaida.auth0.com
+  AUTH0_DOMAIN: '[AUTHO_DOMAIN]', // e.g., kmaida.auth0.com
   AUTH0_API_AUDIENCE: '[http://localhost:8083/api/]', // e.g., 'http://localhost:8083/api/'
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://bradyhouse:4414!aJS$@ds121696.mlab.com:21696/mean-db'
+  MONGO_URI: process.env.MONGO_URI || 'MONGO_DB_CLOUD_URI'
 };
