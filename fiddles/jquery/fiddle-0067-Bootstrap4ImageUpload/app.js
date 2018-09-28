8ba450ba9550f2fc366b0f638a4dc811932cbe26
e13542 <brady.houseknecht@cmegroup.com>
2018-09-28 05:33:31 -0500

    201809280533

--- fiddles/jquery/fiddle-0067-Bootstrap4ImageUpload/app.js
+++ fiddles/jquery/fiddle-0067-Bootstrap4ImageUpload/app.js
@@ -2,17 +2,26 @@
     "use strict";
 
     app.view = app.view || {
-        render: function (hook) {
-            hook.append('hi there!');
-        },
-        init: function () {
-            var hook = $('#fiddleHook');
-            this.render(hook);
+        renderImage: function (obj) {
+          let fileList = obj.files;
+          for (var i = 0; i < fileList.length; i++) {
+            let file = fileList[i],
+              img = document.createElement("img");
+
+            document.getElementById('fiddleHook').appendChild(img);
+            let reader = new FileReader();
+            reader.onload = (function (aImg) {
+              return function (e) {
+                aImg.src = e.target.result;
+              };
+            })(img);
+            reader.readAsDataURL(file);
+          }
         }
     };
     $(document).ready(function () {
-      $('#imageUpload').change(function(x) {
-        alert(x);
+      $('#imageUpload').change(function(file) {
+            app.view.renderImage(file);
       });
     });
 })(window.app = window.app || {}, jQuery)
