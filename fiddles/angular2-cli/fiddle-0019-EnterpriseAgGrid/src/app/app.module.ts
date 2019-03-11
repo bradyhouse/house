e2478c8d83ac3ba48207bfaf40fb86a0cc6f2945
Houseknecht, Brady <bradyhouse@gmail.com>
2019-03-11 15:24:15 -0500

    201903111524

--- fiddles/angular2-cli/fiddle-0019-EnterpriseAgGrid/src/app/app.module.ts
+++ fiddles/angular2-cli/fiddle-0019-EnterpriseAgGrid/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
