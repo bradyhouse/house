7bbbf00f142ff69e1531c4507b0207bb3ff61405
bradyhouse@gmail.com <bradyhouse@gmail.com>
2019-03-26 07:29:55 -0500

    201903260707

--- fiddles/react/fiddle-0015-Portfolio/src/App.js
+++ fiddles/react/fiddle-0015-Portfolio/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import Content from './components/content/content';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Content/>
         <Footer/>
       </div>
     );
