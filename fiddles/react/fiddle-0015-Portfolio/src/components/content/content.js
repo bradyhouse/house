34fe5b23faa57060188b466c09c0b07d42d3c61c
bradyhouse@gmail.com <bradyhouse@gmail.com>
2019-04-10 16:58:23 -0500

    201904090639

--- fiddles/react/fiddle-0015-Portfolio/src/components/content/content.js
+++ fiddles/react/fiddle-0015-Portfolio/src/components/content/content.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
+import Modal from 'react-responsive-modal';
 import './content.css';
-
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
-
-Modal.setAppElement('#root');
 
 
 class Content extends Component {
@@ -35,102 +22,87 @@ class Content extends Component {
     this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal(num, canvas) {
+  state = {
+    open: false,
+  };
+
+  openModal = (num, canvas) => {
     this.num = num;
     this.canvas = canvas;
-    this.setState({modalIsOpen: true});
-  }
+    this.setState({ open: true });
+  };
 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
+  closeModal = () => {
+    this.setState({ open: false });
+  };
+
 
   render() {
+    const { open } = this.state;
     return (
       <div>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-
-          <div className="container">
-            <img height={this.canvasHeight} src={this.canvas} alt="Canvas #{48}"></img>
+        <Modal open={open} onClose={this.closeModal} center>
+          <div className="container autoZoom">
+            <img height={this.canvasHeight} src={this.canvas} alt="Canvas #${this.num}"></img>
           </div>
         </Modal>
-        <div className="content" tabIndex="-1" role="group">
+        <div className="content autoZoom" tabIndex="-1" role="group">
           <figure className="figure">
             <svg id="figure1">
               <image id="48"
                      xlinkHref="images/0048.jpg"
-                     width="105" height="140" x="0" y="0" opacity="1"
+                     width="105" height="140" x="728" y="0" opacity="1"
                      onClick={() => this.openModal("48","images/0048.jpg")}
-                     >
-                <animate attributeName="opacity" dur="229s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="0;728"
+              >
+                <animate attributeName="x" dur="60s" values="728;0"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="49"
                      xlinkHref="images/0049.jpg"
-                     width="105" height="140" x="-88" y="0" opacity="1"
+                     width="105" height="140" x="640" y="0" opacity="1"
                      onClick={() => this.openModal("49","images/0049.jpg")}
               >
-                <animate attributeName="opacity" dur="146s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-88;640"
+                <animate attributeName="x" dur="60s" values="640;-88"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="50"
                      xlinkHref="images/0050.jpg"
-                     width="105" height="140" x="-176" y="0" opacity="1"
+                     width="105" height="140" x="552" y="0" opacity="1"
                      onClick={() => this.openModal("50","images/0050.jpg")}
               >
-                <animate attributeName="opacity" dur="192s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-176;552"
+                <animate attributeName="x" dur="60s" values="552;-176"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="51"
                      xlinkHref="images/0051.jpg"
-                     width="105" height="140" x="-264" y="0" opacity="1"
+                     width="105" height="140" x="464" y="0" opacity="1"
                      onClick={() => this.openModal("51","images/0051.jpg")}
               >
-                <animate attributeName="opacity" dur="237s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-264;464"
+                <animate attributeName="x" dur="60s" values="464;-264"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="52"
                      xlinkHref="images/0052.jpg"
-                     width="105" height="140" x="-352" y="0" opacity="1"
+                     width="105" height="140" x="376" y="0" opacity="1"
                      onClick={() => this.openModal("52","images/0052.jpg")}
               >
-                <animate attributeName="opacity" dur="60s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-352;376"
+                <animate attributeName="x" dur="60s" values="376;-352"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="53"
                      xlinkHref="images/0053.jpg"
-                     width="105" height="140" x="-440" y="0" opacity="1"
+                     width="105" height="140" x="288" y="0" opacity="1"
                      onClick={() => this.openModal("53","images/0053.jpg")}
               >
-                <animate attributeName="opacity" dur="148s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-440;288"
+                <animate attributeName="x" dur="60s" values="288;-440"
                          repeatCount="indefinite"></animate>
               </image>
               <image id="0054-release.jpg"
                      xlinkHref="images/0054.jpg"
-                     width="105" height="140" x="-528" y="0" opacity="1"
+                     width="105" height="140" x="200" y="0" opacity="1"
                      onClick={() => this.openModal("54","images/0054.jpg")}
               >
-                <animate attributeName="opacity" dur="169s" values="1;.9;.8;.7;.6;.5;.6;.7;.8;.9"
-                         repeatCount="indefinite"></animate>
-                <animate attributeName="x" dur="60s" values="-528;200"
+                <animate attributeName="x" dur="60s" values="200;-528"
                          repeatCount="indefinite"></animate>
               </image>
             </svg>
