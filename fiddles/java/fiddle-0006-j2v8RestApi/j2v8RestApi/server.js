bc5e72099539bb29d539b1f85b043b0994b2a675
Houseknecht, Brady <Brady.Houseknecht@cmegroup.com>
2019-10-10 06:02:23 -0500

    201910100602

--- fiddles/java/fiddle-0006-j2v8RestApi/j2v8RestApi/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
- response.writeHead(200, {'Content-Type': 'text/plain'});
- response.write('hi mom!');
- response.end();
-});
-
-server.listen(8000);
-console.log('Server running at https://127.0.0.1:8000/');
