e57bd013f30738f1923a310ca0711e4ed2e7f470
bradyhouse@gmail.com <bradyhouse@gmail.com>
2019-10-20 16:25:05 -0500

    201910201556

---
+++ CHANGELOG-update.md
@@ -0,0 +1,5 @@
+### 324_AngularCli_30 ~ [Issue 324](https://github.com/bradyhouse/house/issues/324)
+
+* **Angular-Cli**
+
+  1. Added [fiddles/angular2-cli/fiddle-0030-EntAgGridChartRangeSelect](fiddles/angular2-cli/fiddle-0030-EntAgGridChartRangeSelect)
---
+++ fiddles/angular2-cli/fiddle-0030-EntAgGridChartRangeSelect/src/app/ui.ts
@@ -0,0 +1,65 @@
+export function createButton(text: string, action: EventListenerOrEventListenerObject): HTMLButtonElement {
+    const button = document.createElement('button');
+    button.textContent = text;
+    document.body.appendChild(button);
+    button.addEventListener('click', action);
+    return button;
+}
+
+export function createSlider<D>(text: string, values: D[], action: (value: D) => void): HTMLInputElement {
+    const n = values.length;
+    const id = String(Date.now());
+    const sliderId = 'slider-' + id;
+    const datalistId = 'slider-list-' + id;
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'inline-flex';
+    wrapper.style.alignItems = 'center';
+    wrapper.style.width = '300px';
+    wrapper.style.padding = '5px';
+    wrapper.style.margin = '5px';
+    wrapper.style.border = '1px solid lightgray';
+    wrapper.style.borderRadius = '5px';
+    wrapper.style.backgroundColor = 'white';
+
+    const slider = document.createElement('input');
+    slider.setAttribute('id', sliderId);
+    slider.setAttribute('list', datalistId);
+    slider.style.height = '1.8em';
+    slider.style.flex = '1';
+
+    const label = document.createElement('label');
+    label.setAttribute('for', sliderId);
+    label.innerHTML = text;
+    label.style.font = '12px sans-serif';
+    label.style.marginRight = '5px';
+
+    // Currently, no browser fully supports these features.
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+    const datalist = document.createElement('datalist');
+    datalist.setAttribute('id', datalistId);
+
+    values.forEach((value, index) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', String(index));
+        option.setAttribute('label', String(value));
+        datalist.appendChild(option);
+    });
+
+    slider.type = 'range';
+    slider.min = '0';
+    slider.max = String(n - 1);
+    slider.step = '1';
+    slider.value = '0';
+    slider.style.width = '200px';
+
+    wrapper.appendChild(label);
+    wrapper.appendChild(slider);
+    wrapper.appendChild(datalist);
+    document.body.appendChild(wrapper);
+
+    slider.addEventListener('input', (e) => {
+        const index = +(e.target as HTMLInputElement).value;
+        action(values[index]);
+    });
+    return slider;
+}
