8eeb8b580bb4601c68f6c574f190d939356890be
bradyhouse@gmail.com <bradyhouse@gmail.com>
2019-11-26 07:20:37 -0500

    201911260720

--- fiddles/angular2-cli/fiddle-0031-NgxBootstrapBasics/dist/NgxBootstrapModal/main-es5.js
+++ fiddles/angular2-cli/fiddle-0031-NgxBootstrapBasics/dist/NgxBootstrapModal/main-es5.js
@@ -1169,7 +1169,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/bradyhouse/github/house_326/fiddles/angular2-cli/fiddle-0031-NgxBootstrapBasics/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/bradyhouse/github/house_master/fiddles/angular2-cli/fiddle-0031-NgxBootstrapBasics/src/main.ts */"./src/main.ts");
 
 
 /***/ })
