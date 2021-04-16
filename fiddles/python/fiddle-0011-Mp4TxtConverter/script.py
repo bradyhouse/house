dcb543c5dc8428562b4bc7ec315be8abc71fdf7d
bradyhouse@gmail.com <bradyhouse@gmail.com>
2021-04-16 12:56:44 -0500

    202104161256

--- fiddles/python/fiddle-0011-Mp4TxtConverter/script.py
+++ fiddles/python/fiddle-0011-Mp4TxtConverter/script.py
@@ -1,10 +1,37 @@
 import os
 import logging
+import time
+import speech_recognition as speech_toolbox
+
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+tmp_dir = 'chunks'
+formats_to_delete = ['.wav']
+transcript_file = 'transcript.txt'
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from pydub import AudioSegment
+
+def cleanup():
+    if os.path.isdir(tmp_dir):
+        os.system('rm -rf ' + tmp_dir)
+    for (dirpath, dirnames, filenames) in os.walk('.'):
+        for filename in filenames:
+            if filename.endswith(tuple(formats_to_delete)):
+                filepath = dirpath + '/' + filename
+                os.system('rm -rf ' + filepath)
+
+
+def setup():
+    if os.path.isfile(transcript_file):
+        os.system('rm -rf ' + transcript_file)
+    if os.path.isdir(tmp_dir):
+        cleanup()
+    os.mkdir(tmp_dir)
+    logger.debug( ' Created ' + tmp_dir +' directory ')
+
 
 def toWav():
     formats_to_convert = ['.m4a']
@@ -20,66 +47,68 @@ def toWav():
                     wav_filename = filename.replace(file_extension_final, 'wav')
                     wav_path = dirpath + '/' + wav_filename
                     file_handle = track.export(wav_path, format='wav')
-                    logger.info('Created: ' + str(wav_filename))
+                    logger.debug(' Created ' + str(wav_filename))
                 except Exception as error:
                     logger.exception(error)
 
-
-from pydub.silence import split_on_silence
 
 def explodeWav():
-    # split into seperate wav files based on pauses and silence level
-    # delete the original wav file
     formats_to_convert = ['.wav']
     for (dirpath, dirnames, filenames) in os.walk('.'):
         for filename in filenames:
             if filename.endswith(tuple(formats_to_convert)):
-                file_path = dirpath + '/' + filename
-                logger.info('Exploding ' + file_path + ' ...')
                 try:
+                    file_path = dirpath + '/' + filename
                     sound_file = AudioSegment.from_wav(file_path)
-                    audio_chunks = split_on_silence(sound_file,
-                        min_silence_len=100,
-                        silence_thresh=-16
-                    )
-                    for i, chunk in enumerate(audio_chunks):
-                        chunk_file = 'chunk{0}.wav'.format(i)
-                        logger.info('Created: ' + str(chunk_file))
-                        chunk.export(chunk_file, format="wav")
-                    os.remove(file_path)
+                    audio_chunks = split_on_silence(sound_file, min_silence_len=2000, silence_thresh=-70)
+                    logger.debug('audio chunk count ' + str(len(audio_chunks)))
+                    if len(audio_chunks) > 1:
+                      for i in range(0, len(audio_chunks)):
+                          time_stamp = time.time()
+                          chunk_file =  os.path.join(tmp_dir, str(i+1).zfill(4)+ '_' +  str(int(time_stamp)) + '.wav')
+                          audio_chunks[i].export(chunk_file, format='wav')
+                          time.sleep(1)
                 except Exception as error:
                     logger.exception(error)
 
-
-import speech_recognition as sr
 
 def toTxt():
-    recognize = sr.Recognizer()
+    recognizer = speech_toolbox.Recognizer()
     formats_to_convert = ['.wav']
-    for (dirpath, dirnames, filenames) in os.walk('.'):
+    for (dirpath, dirnames, filenames) in os.walk('./' + tmp_dir):
+        filenames.sort()
         for filename in filenames:
             if filename.endswith(tuple(formats_to_convert)):
-                filepath = dirpath + '/' + filename
-                (path, file_extension) = os.path.splitext(filepath)
-                file_extension_final = file_extension.replace('.', '')
-
                 try:
-                  with sr.AudioFile(filepath) as source:
-                      audio_data = recognize.record(source)
-                      text = recognize.recognize_google(audio_data)
-                      txt_filename = filename.replace(file_extension_final, 'txt')
-                      txt_path = dirpath + '/' + txt_filename
-                      with open(txt_path, 'w') as txt_file:
-                        print(text, file=txt_file)
-                      logger.info('Created ' + txt_path)
+                    filepath = dirpath + '/' + filename
+                    (path, file_extension) = os.path.splitext(filepath)
+                    file_extension_final = file_extension.replace('.', '')
+                    with speech_toolbox.AudioFile(filepath) as audio_file:
+                        audio_recording = recognizer.record(audio_file)
+                        text = recognizer.recognize_google(audio_recording)
+                        txt_filename = filename.replace(file_extension_final, 'txt')
+                        txt_file_path = dirpath + '/' + txt_filename
+                        with open(txt_file_path, 'w') as txt_file:
+                            print(text, file=txt_file)
+                        logger.debug(' Created ' + txt_file_path)
+                        os.system('cat ' + txt_file_path + ' >> ' + transcript_file)
+                except speech_toolbox.UnknownValueError:
+                    logger.info(' Google Speech Recognition could not understand audio')
+                except speech_toolbox.RequestError as request_error:
+                    logger.info(' Could not request results from Google Speech Recognition service; {0}'.format(request_error))
+                except Exception as unknown_error:
+                    logger.exception(unknown_error)
+    if os.path.isfile(transcript_file):
+        os.system('cat ' + transcript_file)
 
-                except Exception as error:
-                    logger.exception(error)
 
 def main():
+    setup()
     toWav()
     explodeWav()
     toTxt()
+    cleanup()
+
 
 main()
 os._exit(1)
