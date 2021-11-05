8a03164378a7f1a9d4f2369dab75e851f81349de
bradyhouse@gmail.com <bradyhouse@gmail.com>
2021-11-05 06:51:45 -0500

    202111050650

--- fiddles/react/fiddle-0017-EnterpriseAgGrid/setup-jasmine-env.js
+++
@@ -1,9 +0,0 @@
-var jasmineReporters = require('jasmine-reporters');
-jasmine.VERBOSE = true;
-jasmine.getEnv().addReporter(
-  new jasmineReporters.JUnitXmlReporter({
-    consolidateAll: true,
-    savePath: "output/",
-    filePrefix: "test-results"
-  })
-);
