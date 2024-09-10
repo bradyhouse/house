f4ae5e75b774c6f431a34a97af83f05a194a5b53
brady house <bradyhouse@gmail.com>
2024-09-10 08:10:16 -0400

    closes #1997

---
+++ fiddles/vue/fiddle-0016-AgGridContextMenuIconJs/src/components/agrid/AgridContextLinkRenderer.vue
@@ -0,0 +1,40 @@
+<template>
+  <span class="cell-content">
+    {{ params.value }}
+    <span class="icon" @click="onClick($event)">
+      <font-awesome-icon
+        :icon="['fas', 'ellipsis-vertical']"
+        @click="onClick($event)"
+        style="cursor: pointer !important"
+      />
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick(event) {
+      this.params.context.componentParent.showContextMenu(this.params, event)
+    }
+  }
+}
+</script>
+
+<style>
+.cell-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.icon {
+  cursor: pointer !important;
+  margin-left: 8px;
+  visibility: hidden !important;
+}
+
+.cell-content:hover .icon {
+  visibility: visible !important;
+}
+</style>
