77724ddd52da4547bd7b96f29dc4415b77788ff2
brady house <bradyhouse@gmail.com>
2025-04-02 11:54:46 -0400

    closes #2556

---
+++ fiddles/vue/fiddle-0018-InfiniteSelect/src/components/icons/__tests__/IconVueSelect.spec.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import IconVueSelect from '@/components/icons/IconVueSelect.vue'
+
+describe('Icon Vue Select', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(IconVueSelect)
+    expect(wrapper.html()).toContain('img')
+  })
+});
